271ab7f5d97999b742e7e0e76d9e24630c4be57a
Max Rusin <maxunris@gmail.com>
2024-06-25 23:58:13 +0300

    add test "add_new_pet"

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,24 +1,28 @@
-import pytest
 import requests
-import json
-from tests.base import add_new_pet
+import pytest
+from tests.base import add_new_pet, main_headers, update_pet
 from schemas.add_pet_schema import add_pet
 from jsonschema import validate
+from utils.requests_helper import api_request
 
 
-def test_add_new_pet():
-    url = 'https://petstore.swagger.io/v2/pet'
+def test_add_new_pet(base_api_url):
+
+    endpoint = "pet"
     payload = add_new_pet()
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    response = requests.request("POST", url=url, headers=headers, data=payload)
+    headers = main_headers()
+    response = api_request(base_api_url, endpoint,"POST", headers=headers, data=payload)
+
 
     print(response.text)
     assert response.status_code == 200
+    assert response.json()['status'] == "available"
     validate(response.json(), add_pet)
+    return response.json()['id']
+def test_update_pet(base_api_url):
+    endpoint = "pet"
+    payload = update_pet()
+    headers = main_headers()
+    response = api_request(base_api_url, endpoint,"POST", headers=headers, data=payload)
+    assert response.status_code == 200
 
-
-
-def update_pet():
-    pass
