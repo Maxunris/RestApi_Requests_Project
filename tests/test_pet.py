bce6a78ba9122b89441986f6d3f550e778f704b2
Max Rusin <maxunris@gmail.com>
2024-06-26 17:35:41 +0300

    update tests

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,28 +1,31 @@
-import requests
-import pytest
-from tests.base import add_new_pet, main_headers, update_pet
+from tests.base import add_new_pet, main_headers
 from schemas.add_pet_schema import add_pet
 from jsonschema import validate
 from utils.requests_helper import api_request
 
 
 def test_add_new_pet(base_api_url):
+    global main_id
 
     endpoint = "pet"
     payload = add_new_pet()
     headers = main_headers()
-    response = api_request(base_api_url, endpoint,"POST", headers=headers, data=payload)
-
+    response = api_request(base_api_url, endpoint, "POST", headers=headers, data=payload)
 
     print(response.text)
     assert response.status_code == 200
     assert response.json()['status'] == "available"
     validate(response.json(), add_pet)
-    return response.json()['id']
-def test_update_pet(base_api_url):
-    endpoint = "pet"
-    payload = update_pet()
-    headers = main_headers()
-    response = api_request(base_api_url, endpoint,"POST", headers=headers, data=payload)
-    assert response.status_code == 200
 
+    main_id = response.json()['id']
+
+
+def test_update_pet(base_api_url):
+    payload = {"id": main_id, "category": {"id": 0, "name": "string"}, "name": "cat", "photoUrls": ["string"],
+               "tags": [{"id": 0, "name": "string"}], "status": "available"}
+    endpoint = "pet"
+    headers = main_headers()
+    response = api_request(base_api_url, endpoint, "POST", headers=headers, json=payload)
+    assert response.status_code == 200
+    print(response.text)
+    print(payload)
--- utils/requests_helper.py
+++ utils/requests_helper.py
@@ -1,6 +1,7 @@
 import requests
 
-def api_request(base_api_url, endpoint, method, data=None, params=None, headers=None):
+
+def api_request(base_api_url, endpoint, method, data=None, json=None, params=None, headers=None):
     url = f"{base_api_url}{endpoint}"
-    response = requests.request(method, url, data=data, params=params, headers=headers)
+    response = requests.request(method, url, data=data, json=json, params=params, headers=headers)
     return response
