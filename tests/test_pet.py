bb492ee73c6bb52b958eb222574640189ca15662
Max Rusin <maxunris@gmail.com>
2024-06-26 23:33:27 +0300

    add  all pozitive tests

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -3,8 +3,6 @@ from schemas.pet_schema import pet_schema
 from jsonschema import validate
 from utils.requests_helper import api_request
 import pytest
-import requests
-
 
 def test_add_new_pet(base_api_url):
     global main_id
@@ -14,7 +12,6 @@ def test_add_new_pet(base_api_url):
     headers = main_headers()
     response = api_request(base_api_url, endpoint, "POST", headers=headers, data=payload)
 
-    print(response.text)
     assert response.status_code == 200
     assert response.json()['status'] == "available"
     validate(response.json(), pet_schema)
@@ -31,23 +28,16 @@ def test_update_pet(base_api_url):
     assert response.status_code == 200
     validate(response.json(), pet_schema)
     assert response.json()['name'] == "cat"
-    print(response.text)
-
 
 
 def test_find_pet_by_id(base_api_url):
-
     endpoint = (f"pet/{main_id}")
     headers = main_headers()
-    response = api_request(base_api_url, endpoint,"GET", headers=headers)
+    response = api_request(base_api_url, endpoint, "GET", headers=headers)
     assert response.status_code == 200
     validate(response.json(), pet_schema)
     assert response.json()['id'] == main_id
     assert response.json()['name'] == "cat"
-    print(response.text)
-
-
-
 
 
 def test_update_with_form_data(base_api_url):
@@ -56,35 +46,41 @@ def test_update_with_form_data(base_api_url):
         'name': 'Marina',
         'status': 'Switty'
     }
-    headers = main_headers()
-    response = api_request(base_api_url, endpoint,"POST", data=payload)
+    response = api_request(base_api_url, endpoint, "POST", data=payload)
     assert response.status_code == 200
-    assert response.json()['code'] == 200
     assert response.json()['message'] == (f'{main_id}')
-    print(response.text)
-
-
-
-
-
 
 
 @pytest.mark.parametrize(
     "params",
     [{'status': 'available'},
-    {'status': 'pending'},
-    {'status': 'sold'}],
-    ids =["available status", "pending status", "sold status"]
+     {'status': 'pending'},
+     {'status': 'sold'}],
+    ids=["available status", "pending status", "sold status"]
 )
 def test_find_pet_by_status(base_api_url, params):
     endpoint = "pet/findByStatus"
     headers = main_headers()
-    response = api_request(base_api_url, endpoint,"GET", params=params, headers=headers)
+    response = api_request(base_api_url, endpoint, "GET", params=params, headers=headers)
     assert response.status_code == 200
 
     for pet in response.json():
         assert pet['status'] == params['status']
 
-    print(response.text)
 
 
+def test_delete_pet(base_api_url):
+    endpoint = (f"pet/{main_id}")
+    headers = main_headers()
+    response = api_request(base_api_url, endpoint, "DELETE", headers=headers)
+    assert response.status_code == 200
+    assert response.json()['message'] == (f'{main_id}')
+
+
+def test_find_pet_by_id_after_delete(base_api_url):
+    endpoint = (f"pet/{main_id}")
+    headers = main_headers()
+    response = api_request(base_api_url, endpoint, "GET", headers=headers)
+    assert response.status_code == 404
+    assert response.json()['type'] == 'error'
+    assert response.json()['message'] == 'Pet not found'
